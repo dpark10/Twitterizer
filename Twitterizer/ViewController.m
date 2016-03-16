3746f54abf329c48f47319a5936da3e96f57c1d7
Scot Robnett <scotrobnett@Scot-Robnetts-MacBook-Pro.local>
2016-03-16 15:24:12 -0500

    Trying to Replace Vowels

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -28,7 +28,7 @@
     
     for (int i = 0; i < length; i++) {
         if ([twitterText characterAtIndex:i] == @"a" || @"e" || @"i" || @"o" || @"u" || @"y") {
-            NSLog(@"test");
+            
         }
     }
 }
