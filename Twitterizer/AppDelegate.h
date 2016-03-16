5307dfa7b8f4c472dc893fdd8a5e314921fda646
dp <dp@dp.local>
2016-03-16 13:56:21 -0500

    Initial Commit

---
+++ Twitterizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitterizer
+//
+//  Created by dp on 3/16/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitterizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitterizer
+//
+//  Created by dp on 3/16/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
